992200f3dfbbf6cd184af0849f787906d2d1d872
Alec Deason <alecwd@uw.edu>
2018-07-20 13:58:08 -0700

    Add some job utilities

--- src/hierachical_aggregator/hold.py
+++ src/hierachical_aggregator/hold.py
@@ -6,11 +6,13 @@ class Semaphore:
         self._name = name
         self._redis = redis.Redis(host=host, port=port)
 
-    def add_dependencies(self, job_ids):
-        self._redis.sadd(self._name, *job_ids)
+    def add_dependencies(self, *dependency_ids):
+        for dependency_id in dependency_ids:
+            self._redis.sadd(self._name, dependency_id)
 
-    def clear_dependency(self, job_id):
-        self._redis.srem(self._name, job_id)
+    def clear_dependencies(self, *dependency_ids):
+        for dependency_id in dependency_ids:
+            self._redis.srem(self._name, dependency_id)
 
     def is_clear(self):
         return self._redis.scard(self._name) == 0
---
+++ src/hierachical_aggregator/job.py
@@ -0,0 +1,34 @@
+from hierachical_aggregator.message_broker import MessageSchema, RedisMessageQueue
+from hierachical_aggregator.hold import Semaphore
+
+JOB_SCHEMA = MessageSchema({"node_name": str, "parent_node_name": str})
+
+
+def create_job(node_name, parent_node_name, children, host, port):
+    sem = Semaphore(node_name, host, port)
+    sem.add_dependencies(*children)
+    q = RedisMessageQueue(JOB_SCHEMA, "job_queue", host, port)
+    q.send({"node_name": node_name, "parent_node_name": parent_node_name})
+
+
+def get_job(host, port):
+    q = RedisMessageQueue(JOB_SCHEMA, "job_queue", host, port)
+    while True:
+        message = q.recieve(block=False)
+        if message is None:
+            return None
+        sem = Semaphore(message["node_name"], host, port)
+        if sem.is_clear():
+            return message
+        else:
+            q.send(message)
+
+
+def finish_job(node_name, parent_node_name, host, port):
+    sem = Semaphore(parent_node_name, host, port)
+    sem.clear_dependencies(node_name)
+
+
+def give_up_job(node_name, parent_node_name, host, port):
+    q = RedisMessageQueue(JOB_SCHEMA, "job_queue", host, port)
+    q.send({"node_name": node_name, "parent_node_name": parent_node_name})
